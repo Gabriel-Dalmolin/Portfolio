6938320326cc727bfa9f3d85885dcea2722ddcfc
Gabriel <gabrielsomacaldalmolin@gmail.com>
2025-01-06 16:26:28 -0300

    Added another contact options

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 contactWindow = document.getElementById("contact-window")
+contactButton = document.getElementById("contact-button")
 
 function openContactMe(){
     contactWindow.style.display = "block";
     contactWindow.style.visibility = "visible";
+    contactButton.textContent = "Close";
 } function closeContactMe() {
     contactWindow.style.display = "none";
     contactWindow.style.visibility = "hidden";
+    contactButton.textContent = "Contact me"
 }
 
 function handleContactClick(){
